49f458446d19beb313fa575f80a80fb58d0a7dee
FarhanHermawan <farhanhermawan7@gmail.com>
2023-08-02 06:05:18 +0700

    form review with bootstraps

--- js/product.js
+++ js/product.js
@@ -1,12 +1,12 @@
 let ratingStarInput = [...document.querySelectorAll('.rating-star')];
 
-ratingStarInput.map((star, i) =>{
+ratingStarInput.map((star, index) => {
     star.addEventListener('click', () => {
         for(let i = 0; i < 5; i++) {
             if(i <= index){
-                ratingStarInput[i].src = 'img/star-fill.png';
+                ratingStarInput[i].src = 'img/star fill.png';
             } else{
-                ratingStarInput[i].src = 'img/star.png';
+                ratingStarInput[i].src = 'img/star-no-fill.png';
             } 
         }
     })
